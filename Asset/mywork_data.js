8f8e403e4ffd57d315c5a8f15e6f02ab93116ded
unknown <tochidan11@gmail.com>
2025-05-24 10:36:42 +0100

    updated my services to reflex what I offer

--- Asset/mywork_data.js
+++ Asset/mywork_data.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const mywork_data = [
   {
     w_no: 1,
-    w_name: "Company Staff Management Blockchain Project",
+    w_name: "Company Staff Management Blockchain Project(Blockchain)",
     w_img: "/company-management-blockchain.png",
     alt: "nft image",
     desc: "This project is a company staff management system built on the blockchain. It allows companies to manage their staff records, attendance, and payroll in a secure and transparent manner.",
@@ -11,7 +11,7 @@ export const mywork_data = [
   },
   {
     w_no: 2,
-    w_name: "CROWD FUNDING PROJECT",
+    w_name: "CROWD FUNDING PROJECT(Blockchain)",
     // w_img: "/crowdfund.svg",
     w_img: "/nft.svg",
 
@@ -19,28 +19,44 @@ export const mywork_data = [
   },
   {
     w_no: 3,
-    w_name: "STAFF MANAGEMENT PROJECT",
+    w_name: "SUPPLY CHAIN MANAGEMENT(Blockchain)",
     // w_img: "/managerproject.svg",
     alt: "staff management image",
     w_img: "/nft.svg",
   },
   {
     w_no: 4,
-    w_name: "Token ICO Marketplace",
+    w_name: "Token ICO Marketplace(Blockchain)",
     // w_img: "/managerproject.svg",
     alt: "token ico  image",
     w_img: "/nft.svg",
   },
   {
     w_no: 5,
-    w_name: "Staking Dapp",
+    w_name: "Staking Dapp(Blockchain)",
     // w_img: "/managerproject.svg",
     alt: "Staking dapp image",
     w_img: "/nft.svg",
   },
   {
     w_no: 6,
-    w_name: "EtherScan Clone",
+    w_name: "EtherScan Clone(Web2 + Blockchain)",
+    // w_img: "/managerproject.svg",
+    alt: "token ico  image",
+    w_img: "/nft.svg",
+  },
+
+  {
+    w_no: 7,
+    w_name: "AI Image Generator",
+    // w_img: "/managerproject.svg",
+    alt: "token ico  image",
+    w_img: "/nft.svg",
+  },
+
+  {
+    w_no: 6,
+    w_name: "Eccomerce (Web2 solution)",
     // w_img: "/managerproject.svg",
     alt: "token ico  image",
     w_img: "/nft.svg",
